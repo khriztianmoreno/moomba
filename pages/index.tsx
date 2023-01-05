afbd1afcdb7344414f29ff3689960f33b2d12ebc
khriztianmoreno <khriztianmoreno@gmail.com>
2023-01-05 15:42:46 -0500

    feat(landing): :sparkles: add seo meta-tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,40 @@ import SectionTwo from '../features/landing/SectionTwo'
 import SectionThree from '../features/landing/SectionThree'
 
 function Home() {
+  const seo = {
+    title: 'Moomba | New Digital Platform Video',
+    description: 'New Digital Platform Video',
+    url: 'https://moomba.vercel.app/',
+    image: '/social_media.png',
+  }
   return (
     <>
       <Head>
         <title>Moomba | New Digital Platform Video</title>
         <meta name="description" content="New Digital Platform Video" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+
+        <meta name="image" content={seo.image} />
+
+        <meta property="og:type" content={'website'} />
+        <meta property="og:url" content={seo.url} />
+        <meta property="og:description" content={seo.description} />
+        <meta property="og:image" content={seo.image} />
+
+        <meta name="twitter:dnt" content="on" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@moomba" />
+        <meta name="twitter:title" content={seo.title} />
+        <meta name="twitter:description" content={seo.description} />
+        <meta name="twitter:image" content={seo.image} />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <main>
         <Hero />
